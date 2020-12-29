31195fccd7eacb85e5e94cfd0beabaaf5f0b82bb
HienNguyen711 <hienminhnguyen711@gmail.com>
2020-12-29 16:08:26 +0200

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,60 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas Slim Shirt',
+            category:'Shirts',
+            image:'/images/p2.jpg',
+            price: 120,
+            brand: 'Adidas',
+            rating: 5.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Nike Slim Shoes',
+            category:'Shoes',
+            image:'/images/p3.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name:'Nike Slim Pant',
+            category:'Pant',
+            image:'/images/p4.jpg',
+            price: 220,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Puma Hat',
+            category:'Hat',
+            image:'/images/p5.jpg',
+            price: 100,
+            brand: 'Puma',
+            rating: 4.0,
+            numReviews: 15,
+            description: 'high quality product'
+        }
+    ]
+};
+export default data;
